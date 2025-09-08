79d21783b2424ecb53602bf171fdbecd4a4f0a04
Aman <amanmishra150306@gmail.com>
2025-09-08 13:10:19 +0530

    add dockerfile

--- backend/text2sql/settings.py
+++ backend/text2sql/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b_zzl5_^c=w3ozva950#yo-*7h&rl(x_xzvz$ed9pzub$ytts_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
